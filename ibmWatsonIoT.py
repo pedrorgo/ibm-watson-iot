777e275e38e2701d46145daa21d28dda54181e9d
pedrorgo <pedro.rg.oliveira@gmail.com>
2018-09-07 16:31:14 +0100

    Parameters and init file

--- ibmWatsonIoT.py
+++ ibmWatsonIoT.py
@@ -5,6 +5,8 @@
 
 import os
 import sys
+import argparse
+import ConfigParser
 
 __version__ = '0.1.1'
 
@@ -21,14 +23,26 @@ Usage: ibm-watson-iot <test string>
 """[1:-1]
     print(help)
 
+def createConfigFile():
+    Config = ConfigParser.ConfigParser()
+    cfgfile = open(".watson.iot.ini",'w')
+
+    Config.add_section('Connection')
+    Config.set('Connection','host',"https://server")
+    Config.set ('Connection','port', 1575)
+    Config.write(cfgfile)
+    cfgfile.close()
+    print "Configuration file created"
 
 def main():
-    args = sys.argv[1:]
-    if not args:
-        print_help()
-        quit(None, 0)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--init", help="Initializes config file", action="store_true")
+    args = parser.parse_args()
+    if args.init:
+        createConfigFile()
+        quit(None,0)
 
-    print("Python IBM Watson IoT {}".format(args))
+    print("Python IBM Watson IoT ")
 
 if __name__ == '__main__':
     main()
